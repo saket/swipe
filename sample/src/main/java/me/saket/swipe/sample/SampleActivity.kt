11b062a3d78da663e4a06fb6a60611596fee52c1
Saket <saket@squareup.com>
2022-03-10 02:28:56 -0500

    Empty project

---
+++ sample/src/main/java/me/saket/swipe/sample/SampleActivity.kt
@@ -0,0 +1,10 @@
+package me.saket.swipe.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SampleActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+}
---
+++ swipe/src/main/java/me/saket/swipe/swipe.kt
@@ -0,0 +1,2 @@
+package me.saket.swipe
+
