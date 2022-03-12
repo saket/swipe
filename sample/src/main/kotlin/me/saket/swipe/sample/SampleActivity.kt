d68e3deaa2a41e37b04f788c35c02ff8555a9f77
Saket <saket@squareup.com>
2022-03-12 01:20:36 -0500

    sample: Remove bottom spacing from TopAppBar

--- sample/src/main/kotlin/me/saket/swipe/sample/SampleActivity.kt
+++ sample/src/main/kotlin/me/saket/swipe/sample/SampleActivity.kt
@@ -72,9 +72,7 @@ class SampleActivity : AppCompatActivity() {
             }
           ) {
             SwipeableBoxPreview(
-              Modifier
-                .navigationBarsPadding()
-                .padding(top = 60.dp)
+              Modifier.navigationBarsPadding()
             )
           }
         }
