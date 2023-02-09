34221ca2944d2a8674136d707c32c4d8389af864
Saket <saket@squareup.com>
2023-02-08 22:21:02 -0500

    Kotlin 1.8.10, compose compiler 1.4.2, material3 1.0.1 and AGP 7.4.1

--- sample/src/main/kotlin/me/saket/swipe/sample/SampleActivity.kt
+++ sample/src/main/kotlin/me/saket/swipe/sample/SampleActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,9 +21,11 @@ import androidx.compose.material.icons.twotone.Snooze
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -30,15 +33,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
-import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.insets.navigationBarsPadding
-import com.google.accompanist.insets.statusBarsPadding
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import me.saket.swipe.SwipeAction
 import me.saket.swipe.SwipeableActionsBox
@@ -50,31 +50,24 @@ class SampleActivity : AppCompatActivity() {
 
     setContent {
       val uiController = rememberSystemUiController()
+      val systemInDarkTheme = isSystemInDarkTheme()
       LaunchedEffect(Unit) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        uiController.setSystemBarsColor(Color.Transparent, darkIcons = true)
+        uiController.setSystemBarsColor(Color.Transparent, darkIcons = !systemInDarkTheme)
         uiController.setNavigationBarColor(Color.Transparent)
       }
 
-      val colors = lightColorScheme().copy(
-        background = Color.Whisper,
-        surface = Color.Whisper
-      )
+      val colors = if (systemInDarkTheme) dynamicDarkColorScheme(this) else dynamicLightColorScheme(this)
 
       MaterialTheme(colors) {
-        ProvideWindowInsets {
-          Scaffold(
-            topBar = {
-              SmallTopAppBar(
-                modifier = Modifier.statusBarsPadding(),
-                title = { Text("Swipe") }
-              )
-            }
-          ) {
-            SwipeableBoxPreview(
-              Modifier.navigationBarsPadding()
-            )
+        Scaffold(
+          topBar = {
+            TopAppBar(title = { Text(stringResource(R.string.app_name)) })
           }
+        ) { contentPadding ->
+          SwipeableBoxPreview(
+            Modifier.padding(contentPadding)
+          )
         }
       }
     }
@@ -110,7 +103,7 @@ private fun SwipeableBoxPreview(modifier: Modifier = Modifier) {
     startActions = listOf(replyAll),
     endActions = listOf(snooze, archive),
     swipeThreshold = 40.dp,
-    backgroundUntilSwipeThreshold = Color.GraySuit
+    backgroundUntilSwipeThreshold = MaterialTheme.colorScheme.surfaceColorAtElevation(40.dp)
   ) {
     BatmanIpsumItem(
       isSnoozed = isSnoozed
@@ -127,16 +120,15 @@ private fun BatmanIpsumItem(
     modifier
       .fillMaxWidth()
       .shadow(1.dp)
-      .background(Color.White)
-      .padding(20.dp)
+      .background(MaterialTheme.colorScheme.surfaceColorAtElevation(8.dp))
+      .padding(vertical = 16.dp, horizontal = 20.dp)
       .animateContentSize()
   ) {
     Box(
       Modifier
-        .padding(top = 4.dp)
+        .padding(top = 2.dp)
         .size(52.dp)
-        .clip(CircleShape)
-        .background(MaterialTheme.colorScheme.primary)
+        .background(MaterialTheme.colorScheme.primary, CircleShape)
     )
 
     Column(Modifier.padding(horizontal = 16.dp)) {
@@ -145,7 +137,7 @@ private fun BatmanIpsumItem(
         style = MaterialTheme.typography.titleMedium
       )
       Text(
-        modifier = Modifier.padding(top = 2.dp),
+        modifier = Modifier.padding(top = 4.dp),
         text = "Fear is a tool. When that light hits the sky, it’s not just a call. It’s a warning. For them.",
         style = MaterialTheme.typography.bodyMedium
       )
@@ -154,7 +146,7 @@ private fun BatmanIpsumItem(
         Text(
           modifier = Modifier
             .padding(top = 16.dp)
-            .background(Color.SeaBuckthorn.copy(alpha = 0.2f), RoundedCornerShape(4.dp))
+            .background(Color.SeaBuckthorn.copy(alpha = 0.4f), RoundedCornerShape(4.dp))
             .padding(horizontal = 8.dp, vertical = 4.dp),
           text = "Snoozed until tomorrow",
           style = MaterialTheme.typography.labelLarge
@@ -164,8 +156,6 @@ private fun BatmanIpsumItem(
   }
 }
 
-val Color.Companion.Whisper get() = Color(0XFFF8F5FA)
 val Color.Companion.SeaBuckthorn get() = Color(0xFFF9A825)
 val Color.Companion.Fern get() = Color(0xFF66BB6A)
 val Color.Companion.Perfume get() = Color(0xFFD0BCFF)
-val Color.Companion.GraySuit get() = Color(0xFFC1BAC9)
