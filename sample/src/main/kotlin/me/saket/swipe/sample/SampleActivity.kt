49b94226e096fccea6b0c4335c9a596ad66541c2
Saket Narayan <saket@saket.me>
2024-02-03 00:13:24 -0500

    sample: Display a list of swipeable items

--- sample/src/main/kotlin/me/saket/swipe/sample/SampleActivity.kt
+++ sample/src/main/kotlin/me/saket/swipe/sample/SampleActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -72,9 +74,13 @@ class SampleActivity : AppCompatActivity() {
             TopAppBar(title = { Text(stringResource(R.string.app_name)) })
           }
         ) { contentPadding ->
-          SwipeableBoxPreview(
-            Modifier.padding(contentPadding)
-          )
+          LazyColumn(Modifier.padding(contentPadding).fillMaxSize()) {
+            items(20) { index ->
+              SwipeableBoxPreview(
+                Modifier.fillMaxWidth()
+              )
+            }
+          }
         }
       }
     }
